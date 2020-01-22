190e04b8d2d9ee0c27a2002f99099a4b8c0ec45a
Tristan de Boer <info@tristandeboer.nl>
2020-01-22 13:23:59 +0100

    Added torch gpu check

--- detect_image.py
+++ detect_image.py
@@ -68,7 +68,7 @@ class YoloV3:
         self.names_file = names_file
 
         # Select device
-        print(f"Cuda available: {torch.cuda.is_available()}")
+        print("Cuda available: ", torch.cuda.is_available())
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         # Initialize model
